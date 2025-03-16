c59831c06e7a0a1094887c607db6715f1edf364d
skentos1 <kentos.simon@gmail.com>
2025-03-16 20:49:48 +0100

    Removed client .env file from trackingssssss

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white '>
-      <h1 className=' text-3xl font-bold text-[#6082b6]'><Link to='/dashboard'>IntelliHome</Link></h1>
+      <h1 className=' text-3xl font-bold text-[#6082b6]'><Link to='/'>IntelliHome</Link></h1>
       <ul className='hidden md:flex  '>
         <Link to='/' className='p-4 hover:underline'>Domov</Link>
         <Link to='/produkty' className='p-4 hover:underline'>Produkty</Link>
@@ -41,7 +41,7 @@ const Navbar = () => {
     
       <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r z-10 border-r-gray-900  bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] ease-in-out duration-500' :  'ease-in-out duration-500 fixed left-[-100%]'}>
              <ul className='p-4 z-15'>
-                <h1 className=' w-full m-4 text-3xl font-bold text-[#6082b6]'><Link to='/dashboard'>IntelliHome</Link></h1>
+                <h1 className=' w-full m-4 text-3xl font-bold text-[#6082b6]'><Link to='/'>IntelliHome</Link></h1>
                 <li className='uppercase border-b border-gray-600' ><Link to='/' className='block p-4' onClick={handleLinkClick}>Domov</Link></li>
                 <li className='uppercase border-b border-gray-600'><Link to='/produkty' className='block p-4' onClick={handleLinkClick}>Produkty</Link></li>
                 <li className='uppercase border-b border-gray-600'><Link to='/stretnutie' className='block p-4' onClick={handleLinkClick}>Stretnutie</Link></li>
