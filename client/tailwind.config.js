e3bfa8d8a6897eb833408679a2cfbdd288eebe30
skentos1 <kentos.simon@gmail.com>
2025-03-16 20:10:21 +0100

    Initial commit of Intellihomehost project

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      keyframes: {
-        zoomOut: {
-          '0%': { transform: 'scale(1.1)' },
-          '100%': { transform: 'scale(1)' },
-        },
-      },
-      animation: {
-        zoomOut: 'zoomOut 1s ease-out forwards',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
