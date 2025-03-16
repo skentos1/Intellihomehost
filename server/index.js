9782e6a222d2324673f7225530241bccf5357494
skentos1 <kentos.simon@gmail.com>
2025-03-16 20:27:51 +0100

    Removed .env from tracking

--- server/index.js
+++ server/index.js
@@ -13,9 +13,12 @@ dotenv.config();
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/authentification')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((error) => console.error('Error connecting to MongoDB:', error));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch((error) => console.error('Error connecting to MongoDB:', error));
 
 app.use(express.json());
 app.use(cookieParser());
